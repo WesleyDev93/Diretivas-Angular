e492477923aefaed5c707d9c9b22cb532ebf3860
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-14 14:46:47 -0300

    Criando novo componente , e usando ngClass

---
+++ src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributos',
+  templateUrl: './comp-atributos.component.html',
+  styleUrls: ['./comp-atributos.component.css']
+})
+export class CompAtributosComponent {
+
+  estilo: string = "disable";
+
+
+  constructor () {
+
+  }
+
+
+  trocar(){
+    if(this.estilo === "disable"){
+      this.estilo = "enable"
+    }else{
+      this.estilo = "disable"
+    }
+  }
+}
